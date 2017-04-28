e4924751aacaeab397c47beedc79eb38474a5add
Imesh Gunaratne <imesh.gunaratne@gmail.com>
2017-04-28 18:14:28 +0530

    Add create and delete bash scripts

---
+++ all-in-one/create-stack.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+aws cloudformation create-stack \
+--stack-name wso2am-all-in-one \
+--template-body file://WSO2AM210-All-In-One-Deployment.template \
+--parameters ParameterKey=KeyName,ParameterValue=imesh-key1
---
+++ all-in-one/delete-stack.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+aws cloudformation delete-stack --stack-name wso2am-all-in-one
