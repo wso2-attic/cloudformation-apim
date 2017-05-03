8c11beb07f9e38e3931fd5788008bdee4e71a7c1
Imesh Gunaratne <imesh.gunaratne@gmail.com>
2017-05-03 10:45:37 +0530

    Update key name value in create-stack.sh

--- all-in-one/create-stack.sh
+++ all-in-one/create-stack.sh
@@ -3,4 +3,4 @@
 aws cloudformation create-stack \
 --stack-name wso2am-all-in-one \
 --template-body file://WSO2AM210-All-In-One-Deployment.template \
---parameters ParameterKey=KeyName,ParameterValue=imesh-key1
+--parameters ParameterKey=KeyName,ParameterValue=wso2-key
