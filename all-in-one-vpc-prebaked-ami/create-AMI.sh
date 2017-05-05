35edeb8763d6bb0e884a348473f853f31d71c1b6
Chamila de Alwis <chamila@apache.org>
2017-05-05 08:20:23 +0530

    Initial pre-baked AMI scripts

---
+++ all-in-one-vpc-prebaked-ami/create-AMI.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Check if Packer exists
+command -v packer >/dev/null 2>&1 || { echo >&2 "Packer was not found. Please install Packer first."; exit 1; }
+
+pushd AMI
+packer validate am-default.json
+
+if [ $? != 0 ]; then
+  exit 1;
+fi
+
+echo -n "AWS Access Key: "
+read -r access_key
+
+echo -n "AWS Access Secret: "
+read -s secret_key
+echo
+
+packer build -var "aws_access_key=${access_key}" -var "aws_secret_key=${secret_key}" am-default.json
+
+popd
+
+echo "DONE!"
