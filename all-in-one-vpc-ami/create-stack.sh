fb184be1d20f7a74c1ed43b7193986a029d88528
Imesh Gunaratne <imesh.gunaratne@gmail.com>
2017-05-15 10:36:35 +0530

    Update create-stack.sh and delete-stack.sh

--- all-in-one-vpc-ami/create-stack.sh
+++ all-in-one-vpc-ami/create-stack.sh
@@ -15,6 +15,6 @@
 #  limitations under the License.
 
 aws cloudformation create-stack \
---stack-name wso2am-all-in-one-vpc \
+--stack-name wso2am-all-in-one-vpc-ami \
 --template-body file://WSO2AM210-All-In-One-Deployment-VPC.template.yaml \
 --parameters ParameterKey=KeyName,ParameterValue=wso2-key
--- all-in-one-vpc-ami/delete-stack.sh
+++ all-in-one-vpc-ami/delete-stack.sh
@@ -14,4 +14,4 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-aws cloudformation delete-stack --stack-name wso2am-all-in-one-vpc
+aws cloudformation delete-stack --stack-name wso2am-all-in-one-vpc-ami
