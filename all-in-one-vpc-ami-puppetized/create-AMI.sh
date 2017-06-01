1b20d76e9fa4ea0373d368ce7afe7d96363cd88a
Imesh Gunaratne <imesh.gunaratne@gmail.com>
2017-06-01 11:15:21 +0530

    Change create-AMI.sh to use docker

--- all-in-one-vpc-ami-puppetized/create-AMI.sh
+++ all-in-one-vpc-ami-puppetized/create-AMI.sh
@@ -11,20 +11,13 @@ if [ $? != 0 ]; then
   exit 1;
 fi
 
-aws_credentials=""
-if [ ! -e ~/.aws/credentials ]; then
-  echo -n "AWS Access Key: "
-  read -r access_key
+aws_access_key=${AWS_ACCESS_KEY_ID? "Environment variable AWS_ACCESS_KEY_ID is not set"}
+aws_secret_access_key=${AWS_SECRET_ACCESS_KEY? "Environment variable AWS_SECRET_ACCESS_KEY is not set"}
 
-  echo -n "AWS Access Secret: "
-  read -s secret_key
-  echo
-  echo
-
-  aws_credentials="-var \"aws_access_key=${access_key}\" -var \"aws_secret_key=${secret_key}\""
-fi
-
-packer build $aws_credentials am-default.json
+echo "Starting packer build with docker..."
+docker run -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+           -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+           -i -t -v $(pwd):/opt/ -w /opt/ hashicorp/packer:light build /opt/am-default.json
 
 popd > /dev/null 2>&1
 echo "DONE!"
