d0fe1313d8bc48adb7c9b40c20c04148c033fcea
Chamila de Alwis <chamila@apache.org>
2017-06-05 12:26:24 +0530

    Control JDK installation from Puppet facts

--- all-in-one-vpc-ami-puppetized/AMI/scripts/config.sh
+++ all-in-one-vpc-ami-puppetized/AMI/scripts/config.sh
@@ -14,7 +14,7 @@ echo "Installing packages..."
 wget https://apt.puppetlabs.com/puppetlabs-release-trusty.deb
 dpkg -i puppetlabs-release-trusty.deb
 apt-get update
-apt-get install -y unzip cron-apt nfs-common puppet facter zip unzip mysql-client
+apt-get install -y unzip zip cron-apt nfs-common puppet facter mysql-client
 
 echo "Mounting block device..."
 mkfs.ext4 /dev/xvdf
@@ -29,15 +29,15 @@ puppet module install puppetlabs/stdlib
 puppet module install 7terminals-java
 
 echo "Downloading packs..."
-wget --tries=3 --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz -P /etc/puppet/files/packs
-wget --tries=3 --user-agent="testuser" --referer="http://connect.wso2.com/wso2/getform/reg/new_product_download" http://product-dist.wso2.com/products/api-manager/2.1.0/wso2am-2.1.0.zip -P /etc/puppet/modules/wso2am_runtime/files
+wget --tries=3 -q --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz -P /etc/puppet/files/packs
+wget --tries=3 -q --user-agent="testuser" --referer="http://connect.wso2.com/wso2/getform/reg/new_product_download" http://product-dist.wso2.com/products/api-manager/2.1.0/wso2am-2.1.0.zip -P /etc/puppet/modules/wso2am_runtime/files
 
-wget --tries=3 https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.41.zip -P /tmp
-unzip /tmp/mysql-connector-java-5.1.41.zip -d /tmp
+wget --tries=3 -q https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.41.zip -P /tmp
+unzip -q /tmp/mysql-connector-java-5.1.41.zip -d /tmp
 mkdir -p /etc/puppet/modules/wso2am_runtime/files/configs/repository/components/lib
 cp /tmp/mysql-connector-java-5.1.41/mysql-connector-java-5.1.41-bin.jar /etc/puppet/modules/wso2am_runtime/files/configs/repository/components/lib
 
-tar zxvf /etc/puppet/files/packs/jdk-8u131-linux-x64.tar.gz -C /opt
+tar zxf /etc/puppet/files/packs/jdk-8u131-linux-x64.tar.gz -C /opt
 ln -s /opt/jdk1.8.0_131/ /opt/java
 
 cp /tmp/set_java_home.sh /etc/profile.d/set_java_home.sh
@@ -56,8 +56,8 @@ sed -i 's/#networkaddress.cache.ttl=-1/networkaddress.cache.ttl=30/' /opt/java/j
 # JDK Hardening: Install java cryptographic extentions (JCE)
 mv /opt/java/jre/lib/security/local_policy.jar{,.orig}
 mv /opt/java/jre/lib/security/US_export_policy.jar{,.orig}
-wget --tries=3 --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jce/7/UnlimitedJCEPolicyJDK7.zip
-unzip UnlimitedJCEPolicyJDK7.zip
+wget --tries=3 -q --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jce/7/UnlimitedJCEPolicyJDK7.zip
+unzip -q UnlimitedJCEPolicyJDK7.zip
 mv UnlimitedJCEPolicy/*.jar /opt/java/jre/lib/security/
 rm -rf /opt/jdk-7u80-linux-x64.tar.gz /opt/UnlimitedJCEPolicyJDK7.zip /opt/UnlimitedJCEPolicy
 
--- all-in-one-vpc-ami-puppetized/create-AMI.sh
+++ all-in-one-vpc-ami-puppetized/create-AMI.sh
@@ -11,13 +11,31 @@ if [ $? != 0 ]; then
   exit 1;
 fi
 
-aws_access_key=${AWS_ACCESS_KEY_ID? "Environment variable AWS_ACCESS_KEY_ID is not set"}
-aws_secret_access_key=${AWS_SECRET_ACCESS_KEY? "Environment variable AWS_SECRET_ACCESS_KEY is not set"}
+if [ "$OSTYPE" == "linux-gnu" ]; then
+  aws_credentials=""
+  if [ ! -e ~/.aws/credentials ]; then
+    echo -n "AWS Access Key: "
+    read -r access_key
 
-echo "Starting packer build with docker..."
-docker run -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
-           -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
-           -i -t -v $(pwd):/opt/ -w /opt/ hashicorp/packer:light build /opt/am-default.json
+    echo -n "AWS Access Secret: "
+    read -s secret_key
+    echo
+    echo
+
+    aws_credentials="-var \"aws_access_key=${access_key}\" -var \"aws_secret_key=${secret_key}\""
+  fi
+
+  packer build $aws_credentials am-default.json
+
+elif [ "$OSTYPE" == "darwin" ]; then
+  aws_access_key=${AWS_ACCESS_KEY_ID? "Environment variable AWS_ACCESS_KEY_ID is not set"}
+  aws_secret_access_key=${AWS_SECRET_ACCESS_KEY? "Environment variable AWS_SECRET_ACCESS_KEY is not set"}
+
+  echo "Starting packer build with docker..."
+  docker run -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+             -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+             -i -t -v $(pwd):/opt/ -w /opt/ hashicorp/packer:light build /opt/am-default.json
+fi
 
 popd > /dev/null 2>&1
 echo "DONE!"
